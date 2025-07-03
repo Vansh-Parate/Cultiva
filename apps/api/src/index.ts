b72c62dd6da8b46d60e26c285243088579734ddd
Vansh <vanshparate@gmail.com>
2025-07-03 16:03:24 +0530

    auth completed

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,24 @@
 import express, { Request, Response } from 'express'
 import { userRoute } from '@/routes/index'
+import authRoutes  from './routes/auth.route';
+import session from 'express-session';
+import passport from './auth/passport';
 import cors from 'cors'
 import dotenv from 'dotenv'
-dotenv.config({ path: './env' })
+
+dotenv.config({ path: '.env' })
+
 const PORT = process.env.PORT || 6969
 const app = express()
+
 app.use(express.json())
 app.use(cors())
 app.use('/api/v1/users', userRoute)
+app.use('/api/v1/auth', authRoutes);
+app.use(session({ secret:"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", resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (_req: Request, res: Response) => {
   res.status(200).json({
     success: true,
---
+++ apps/api/src/routes/auth.route.ts
@@ -0,0 +1,81 @@
+import express, { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import passport from "@/auth/passport";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router: Router = express.Router();
+const prisma = new PrismaClient();
+
+router.post("/signup", async(req,res) => {
+    const {fullName, email, password} = req.body;
+
+    if(!fullName || !email || !password){
+        return res.status(400).json({error: "All field are required"});
+    }
+
+    const existingUser = await prisma.user.findUnique({where : {email}});
+
+    if(existingUser){
+        return res.status(400).json({ error: "Email already in use"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const user = await prisma.user.create({
+        data: {
+            fullName,
+            email,
+            password: hashedPassword,
+            username: email.split("@")[0]
+        }
+    });
+
+    const token = jwt.sign({ userId : user.id }, process.env.JWT_SECRET!, { expiresIn : "7d"});
+
+    res.status(201).json({
+        token,
+        user: { id: user.id, fullName: user.fullName, email: user.email}
+    });
+});
+
+router.post("/signin", async(req,res) => {
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({ error : "All fields are required"});
+    }
+
+    const user = await prisma.user.findUnique({ where: {email} });
+
+    if(!user){
+        return res.status(401).json({ error: "Invalid credentials." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        res.status(401).json({ error: "Invalid credentials." });
+    }
+
+    const token = jwt.sign({ userId : user.id }, process.env.JWT_SECRET!, {expiresIn : "7d"});
+
+    res.json({
+        token,
+        user: { id: user.id, fullName: user.fullName, email: user.email } 
+    })
+});
+
+router.get("/google", passport.authenticate("google", {scope: ["profile", "email"]}));
+
+router.get(
+    "/google/callback",
+    passport.authenticate("google", {session:false, failureRedirect:"/"}),
+    (req,res) => {
+        const user = req.user as any;
+        const token = jwt.sign({ userId: user.id}, process.env.JWT_SECRET!);
+
+        res.redirect(`http://localhost:5173/auth/google/success?token${token}`);
+    }
+);
+
+export default router;
