3e20e68f6bd4a066647101380c68230bbb213dbe
Vansh <vanshparate@gmail.com>
2025-07-06 18:24:03 +0530

    dashboard

--- apps/web/src/pages/Dashboard.tsx
+++ apps/web/src/pages/Dashboard.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { FaLeaf, FaSun, FaMoon, FaPlus } from "react-icons/fa";
-import Sidebar from "../components/Sidebar";
+
 
 const Dashboard = () => {
   const [darkMode, setDarkMode] = useState(true);
 
   return (
     <div className={darkMode ? "bg-[#18181b] min-h-screen flex" : "bg-white min-h-screen flex"}>
-      <Sidebar darkMode={darkMode} setDarkMode={setDarkMode} />
+      
       
     </div>
   );
