a55beb817c2595a00a6db8e6e1b87c71d583874d
Vansh <vanshparate@gmail.com>
2025-07-11 23:30:25 +0530

    deploy fixes

--- apps/api/src/auth/passport.ts
+++ apps/api/src/auth/passport.ts
@@ -12,7 +12,7 @@ passport.use(
       {
         clientID: process.env.GOOGLE_CLIENT_ID!,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        callbackURL: "https://greencare.onrender.com/api/v1/auth/google/callback",
+        callbackURL: process.env.GOOGLE_CALLBACK_URL!,
       },
       async (accessToken, refreshToken, profile, done) => {
         // Find or create user in your DB
