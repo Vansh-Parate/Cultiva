4b23c4f08a98dd712b18e8b79d1356ea24441fa2
Vansh <vanshparate@gmail.com>
2025-07-11 23:23:32 +0530

    deploy fixes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Signup from './pages/auth/Signup'
 import Signin from './pages/auth/Signin';
 import GoogleSuccess from './pages/auth/GoogleSuccess';
@@ -10,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/auth/signup" replace />} />
         <Route path="/auth/signup" element={<Signup />} />
         <Route path="/auth/signin" element={<Signin />} />
         <Route path="/auth/google/success" element={<GoogleSuccess />} />
