0d9dc5114535ae56433f0fb0996d31a61b81b346
Vansh <vanshparate@gmail.com>
2025-07-11 22:57:22 +0530

    deploy fix

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,9 @@
+import cors from 'cors';
 import express, { Request, Response } from 'express'
 import { userRoute } from '@/routes/index'
 import authRoutes  from './routes/auth.route';
 import session from 'express-session';
 import passport from './auth/passport';
-import cors from 'cors'
 import dotenv from 'dotenv'
 
 dotenv.config({ path: '.env' })
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 6969
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 
 app.use('/api/v1/auth', authRoutes);
 app.use(session({ secret:"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", resave: false, saveUninitialized: false}));
