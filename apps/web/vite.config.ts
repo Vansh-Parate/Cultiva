745cf46968f67214edeb8df1fad8b1e90197a058
Vansh <vanshparate@gmail.com>
2025-07-30 16:09:48 +0530

    deploy fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -18,4 +19,7 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src'),
     },
   },
+  define: {
+    'process.env': {}
+  }
 })
